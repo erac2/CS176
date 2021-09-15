9524d7dbf0e34a3fdfc5ab5ce722d7245ec87637
erac2 <71734077+erac2@users.noreply.github.com>
2021-09-15 14:54:12 -0400

    CashRegister

---
+++ CashRegister.java
@@ -0,0 +1,45 @@
+
+public class CashRegister {
+
+		private double purchase;
+		private double payment;
+		
+		
+	CashRegister()
+	{
+	purchase = 0;
+	payment = 0;
+	}
+
+	/**
+	 * Records the sale of an item
+	 */
+	
+	public void recordPurchase(double cost)
+	{
+		purchase = purchase + cost;
+	}
+	
+	/**
+	 * Processes a payment received from the customer.
+	 */
+	
+	public void receivePayment(double paymentReceived)
+	{
+		payment = payment + paymentReceived;
+	}
+	
+	/**
+	 * Computes the change due and resets the machine for the next customer.
+	 * the change due to the customer
+	 */
+	
+	public double givechange()
+	{
+		double change = purchase - payment;
+		purchase = 0;
+		payment = 0;
+		return change;
+	}
+
+}
---
+++ CashRegisterTester.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class CashRegisterTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner in = new Scanner(System.in);
+		CashRegister CashRegister = new CashRegister();
+		
+		double cost;
+		double payment;
+		double change;
+		
+		System.out.println("Enter a purchase amount, or -1 to stop: ");
+		
+		while (in.hasNextDouble())
+		{
+			double amount = in.nextDouble();
+			if(amount == -1)
+			{
+				break;
+			}
+				CashRegister.recordPurchase(amount);;
+				System.out.println("Enter a purchase amount, or -1 to stop: ");
+		}
+		System.out.println("Your payment: ");
+		
+		payment = in.nextDouble();
+		CashRegister.receivePayment(payment);
+		
+		System.out.println("Your change: " + -CashRegister.givechange());
+	}
+
+}
